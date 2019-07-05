f9f77325b2a579e7c5f79a1bca0f884cf25df6d4
yangrenjiang <you@example.com>
2019-07-05 19:38:07 +0800

    清理不必要代码

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -9,12 +9,3 @@ docker stop  my-001-prometheus
 docker rm  my-001-prometheus
 docker run --name my-001-prometheus -d   -p 9090:9090 -v /usr/local/promethues/prometheus.yml:/etc/prometheus/prometheus.yml  -v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml  prom/prometheus:v2.7.2 --config.file=/etc/prometheus/prometheus.yml --web.enable-lifecycle 
 
-#docker run --name my-001-prometheus -d \
-#-v /usr/local/promethues/prometheus.yml:/etc/prometheus/prometheus.yml \
-#-v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml \
-#docker run --name my-001-prometheus -d   -p 9090:9090  prom/prometheus:v2.7.2 --config.file=/etc/prometheus/prometheus.yml --web.enable-lifecycle \
-#-v prometheus.yml:/etc/prometheus/prometheus.yml \
-#-v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml \
-# prom/prometheus:v2.7.2 \
-#--config.file=/etc/prometheus/prometheus.yml \
-#--web.enable-lifecycle=true
