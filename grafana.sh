54c1c29b3fba281aff65b860e92c241e357ca0e3
yangrenjiang <you@example.com>
2019-07-05 18:49:10 +0800

    初始化prometheus脚本

---
+++ grafana.sh
@@ -0,0 +1,3 @@
+docker stop  my-001-grafana
+docker rm  my-001-grafana
+docker run -d --name my-001-grafana -p 3000:3000 grafana/grafana
---
+++ prometheus/prometheus.sh
@@ -0,0 +1,20 @@
+rm -rf /usr/local/promethues
+mkdir -p /usr/local/promethues
+cp prometheus.yml /usr/local/promethues/
+mkdir -p /usr/local/promethues/server
+mkdir -p /usr/local/promethues/client
+touch /usr/local/promethues/server/rules.yml
+chmod 777 /usr/local/promethues/server/rules.yml
+docker stop  my-001-prometheus
+docker rm  my-001-prometheus
+docker run --name my-001-prometheus -d   -p 9090:9090 -v /usr/local/promethues/prometheus.yml:/etc/prometheus/prometheus.yml  -v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml  prom/prometheus:v2.7.2 --config.file=/etc/prometheus/prometheus.yml --web.enable-lifecycle 
+
+#docker run --name my-001-prometheus -d \
+#-v /usr/local/promethues/prometheus.yml:/etc/prometheus/prometheus.yml \
+#-v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml \
+#docker run --name my-001-prometheus -d   -p 9090:9090  prom/prometheus:v2.7.2 --config.file=/etc/prometheus/prometheus.yml --web.enable-lifecycle \
+#-v prometheus.yml:/etc/prometheus/prometheus.yml \
+#-v /usr/local/promethues/server/rules.yml:/etc/prometheus/rules.yml \
+# prom/prometheus:v2.7.2 \
+#--config.file=/etc/prometheus/prometheus.yml \
+#--web.enable-lifecycle=true
