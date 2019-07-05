d7196d60434627c7929a49d86a33ad911059356d
杨仁将 <yangrenjiang@tibiot.com>
2019-07-05 15:53:49 +0800

    设置容器时区

--- prometheus/alertmanager.sh
+++ prometheus/alertmanager.sh
@@ -5,5 +5,5 @@ sed -i "s/##ip##/$1/g" /usr/local/promethues/altermanager/alertmanager.yml
 chmod 777 /usr/local/promethues/altermanager/*
 docker stop  my-001-prometheus-altermanager
 docker rm  my-001-prometheus-altermanager
-docker run -e TZ="Asia/Shanghai --name my-001-prometheus-altermanager -v /usr/local/promethues/altermanager/alertmanager.yml:/etc/alertmanager/alertmanager.yml -d   -p 9093:9093  prom/alertmanager:v0.17.0
+docker run -e TZ="Asia/Shanghai" --name my-001-prometheus-altermanager -v /usr/local/promethues/altermanager/alertmanager.yml:/etc/alertmanager/alertmanager.yml -d   -p 9093:9093  prom/alertmanager:v0.17.0
 
