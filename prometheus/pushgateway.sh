423064a8f954d66397aa23767f4912491249b296
yrj2011 <470018286@qq.com>
2019-08-14 14:18:30 +0800

    fix

--- prometheus/pushgateway.sh
+++ prometheus/pushgateway.sh
@@ -2,5 +2,5 @@
 mkdir -p /usr/local/promethues/pushgateway
 docker stop  my-001-prometheus-pushgateway
 docker rm  my-001-prometheus-pushgateway
-docker run -v /etc/localtime:/etc/localtime  --name my-001-prometheus-pushgateway -d   -p 9091:9091  prom/pushgateway:v0.8.0 
+docker run   --name my-001-prometheus-pushgateway -d   -p 9091:9091  my/pushgateway:0.8 
 
---
+++ prometheus/pushgateway/delJob.sh
@@ -0,0 +1,1 @@
+curl -X DELETE http://localhost:9091/metrics/job/channel-metrics
