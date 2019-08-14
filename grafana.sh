9a05cb024591a17be2877ec1da40bc04375ef3f8
yrj2011 <470018286@qq.com>
2019-08-14 17:08:00 +0800

    init

--- grafana.sh
+++ grafana.sh
@@ -1,3 +1,3 @@
 docker stop  my-001-grafana
 docker rm  my-001-grafana
-docker run -d --name my-001-grafana -p 3000:3000 grafana/grafana
+docker run  -d --name my-001-grafana -p 3000:3000 grafana/grafana
