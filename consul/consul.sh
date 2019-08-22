d1e5dc93678606e269a3aa2d7ad88edfc62e2649
root <root@localhost.localdomain>
2019-08-22 13:47:15 +0800

    fix

--- consul/consul.sh
+++ consul/consul.sh
@@ -1,3 +1,3 @@
 docker stop  my-001-consul
 docker rm  my-001-consul
-docker run -d -p 8500:8500 -h node1 --name my-001-consul  consul agent -server -bootstrap-expect=1  -node=node1 -client 0.0.0.0 -ui
+docker run  --restart=always  -d -p 8500:8500 -h node1 --name my-001-consul  consul agent -server -bootstrap-expect=1  -node=node1 -client 0.0.0.0 -ui
